9d439f010fad67fef83f5854f0bb8178cfb95ae6
Ted_Blackson <62517419+tewodros-web@users.noreply.github.com>
2022-06-18 16:45:18 -0400

    Update scenes.py

--- scenes.py
+++ scenes.py
@@ -7,41 +7,41 @@ from OpenGL.GL.shaders import *
 from compnents import *
 from plane import *
 
-def draw_scene2(d):
-    glClear(GL_COLOR_BUFFER_BIT)
+def draw_scene2(d,c=300.0):
+    # glClear(GL_COLOR_BUFFER_BIT)
     glPushMatrix()
-    glTranslated(d,300.0,0.0)
+    glTranslated(d,c,0.0)
 
     rectangular_body()
     glPopMatrix()
     glPushMatrix()
-    glTranslated(d,300.0,0.0)
+    glTranslated(d,c,0.0)
 
     const_plane()
     
     glPopMatrix()
     glPushMatrix()
-    glTranslated(d,300.0,0.0)
+    glTranslated(d,c,0.0)
 
     outline_upper_triangle()
     glPopMatrix()
     glPushMatrix()
-    glTranslated(d,300.0,0.0)
+    glTranslated(d,c,0.0)
 
     lower_triangle()
     glPopMatrix()
     glPushMatrix()
-    glTranslated(d,300.0,0.0)
+    glTranslated(d,c,0.0)
 
     back_wing()
     glPopMatrix()
     glPushMatrix()
-    glTranslated(d,300.0,0.0)
+    glTranslated(d,c,0.0)
 
     left_wing()
     glPopMatrix()
     glPushMatrix()
-    glTranslated(d,300.0,0.0)
+    glTranslated(d,c,0.0)
     
     right_wing()
     glPopMatrix()
